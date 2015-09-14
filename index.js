605e5d7960700a2b78d2970be3610ff7092070bf
Matthew 'Skip' Rotter <mrotter@globesherpa.com>
2015-09-14 12:52:13 -0700

    allowing sessionId to be found in header

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ function session(options){
     req.sessionStore = store;
 
     // get the session ID from the cookie
-    var cookieId = req.sessionID = getcookie(req, name, secrets);
+    var cookieId = req.sessionID || (req.sessionID = getcookie(req, name, secret));
 
     // set-cookie
     onHeaders(res, function(){
